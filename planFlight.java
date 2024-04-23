429c61db5913b4080ae1b96eeadbdd40e9500064
Your Name <you@example.com>
2024-04-23 17:04:14 -0500

    add

---
+++ planFlight.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class planFlight{
+class GFG {
+    static class AdjListNode {
+        int vertex, cost, time;
+ 
+        AdjListNode(int v, int c, int t)
+        {
+            vertex = v;
+            cost = c;
+            time = t;
+        }
+        int getVertex() { return vertex; }
+        int getCost() { return cost; }
+        int getTime() { return time; }
+    }
+}
+
+public static void main(String[] args){
+
+    int V = 9;
+    // ArrayList<ArrayList<AdjListNode> > graph
+        // = new ArrayList<>();
+    for (int i = 0; i < V; i++) {
+        // graph.add(new ArrayList<>());
+    }
+    int source = 0;
+    // graph.get(0).add(new AdjListNode(1, 4));
+}
+
+}
